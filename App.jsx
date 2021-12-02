a43605e2ddc4e158697e0b98f5dc75fe0ecab946
Alexey Ignatenko <aleksey.ignatenko@amomedia.com>
2021-12-02 19:07:32 +0200

    refactor

--- App.jsx
+++ App.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>APP=helloworld!!!</div>
+      <div id="over">
+          <span class="Centerer"></span>
+          <img class="Centered" src="demo_pic.png" />
+      </div>
     );
   }
 }
